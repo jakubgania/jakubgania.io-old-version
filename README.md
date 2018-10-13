5b9a4d703f52e4315def8ea76396d292e9df0271
jakubgania <jakubgania@outlook.com>
2018-10-13 11:44:41 +0200

    updated pages/other-resources

--- README.md
+++ README.md
@@ -5,22 +5,3 @@ Project of my personal website which is constantly being expanded.
 You can see -> [jakubgania.io](https://jakubgania.io)
 
 >  
-
-## Build Setup
-
-``` bash
-# install dependencies
-$ npm install
-
-# serve with hot reload at localhost:3000
-$ npm run dev
-
-# build for production and launch server
-$ npm run build
-$ npm start
-
-# generate static project
-$ npm run generate
-```
-
-For detailed explanation on how things work, checkout [Nuxt.js docs](https://nuxtjs.org).
--- pages/other-resources.vue
+++ pages/other-resources.vue
@@ -1,14 +1,14 @@
 <template>
   <v-layout row wrap class="projects-layout">
-    <v-flex lg8 offset-lg2>
-      <div class="image-section" data-aos="zoom-in" data-aos-delay="440" data-aos-duration="800" data-aos-once="true">
+    <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg8 offset-lg2>
+      <div data-aos="zoom-in" data-aos-delay="440" data-aos-duration="800" data-aos-once="true">
         <div class="" style="font-size:42px;margin-top:140px;text-align:center;">
           Inne
         </div>
       </div>
     </v-flex>
-    <v-flex lg8 offset-lg2 style="margin-bottom:80px;">
-      <div class="image-section" data-aos="fade-up" data-aos-delay="440" data-aos-duration="1200" data-aos-once="true">
+    <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg8 offset-lg2 style="margin-bottom:80px;">
+      <div data-aos="fade-up" data-aos-delay="440" data-aos-duration="1200" data-aos-once="true">
         <div class="" style="font-size:24px;margin-top:60px;text-align:center;">
           Na tej podstronie będą pojawiać się linki do różnych stron.
         </div>
@@ -18,6 +18,10 @@
       v-for="(link, index) in linksData"
       :key="link.id"
       xs12
+      sm8
+      offset-sm2
+      md6
+      offset-md3
       lg6
       offset-lg3
     >
@@ -29,12 +33,17 @@
           v-for="(resource, index) in link.resource_addresses"
           :key="resource.id"
           xs12
+          sm12
+          md12
           lg12
         >
           <div class="" style="font-size:18px;padding-bottom:10px;">
-            <a :href="resource.url" target="_blank" style="text-decoration:none;">
-              {{ resource.title ? resource.title : resource.url }}
+            <a :href="resource.url" target="_blank" class="link">
+              {{ resource.title ? index + 1 + ". " + resource.title : index + 1 + ". " + resource.url }}
             </a>
+          </div>
+          <div v-if="resource.description" class="" style="">
+            {{ resource.description }}
           </div>
         </v-flex>
       </v-layout>
@@ -63,5 +72,22 @@ export default {
 .projects-layout
 {
   padding-bottom: 80px;
+}
+.link
+{
+  text-decoration: none;
+
+  &:hover
+  {
+    color: #01aca7;
+  }
+}
+@media only screen and (max-width: 600px)
+{
+  .projects-layout
+  {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
 }
 </style>
