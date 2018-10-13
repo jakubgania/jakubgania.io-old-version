05f3b86172e0bc4348dce8e50035d62756ed8598
jakubgania <jakubgania@outlook.com>
2018-10-13 18:27:39 +0200

    updated pages/other-resources

--- pages/other-resources.vue
+++ pages/other-resources.vue
@@ -1,17 +1,13 @@
 <template>
   <v-layout row wrap class="projects-layout">
     <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg8 offset-lg2>
-      <div data-aos="zoom-in" data-aos-delay="440" data-aos-duration="800" data-aos-once="true">
-        <div class="" style="font-size:42px;margin-top:140px;text-align:center;">
-          Inne
-        </div>
+      <div class="" style="font-size:42px;margin-top:140px;text-align:center;">
+        Inne
       </div>
     </v-flex>
     <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg8 offset-lg2 style="margin-bottom:80px;">
-      <div data-aos="fade-up" data-aos-delay="440" data-aos-duration="1200" data-aos-once="true">
-        <div class="" style="font-size:24px;margin-top:60px;text-align:center;">
-          Na tej podstronie będą pojawiać się linki do różnych stron.
-        </div>
+      <div class="" style="font-size:24px;margin-top:60px;text-align:center;">
+        Na tej podstronie będą pojawiać się linki do różnych stron.
       </div>
     </v-flex>
     <v-flex
