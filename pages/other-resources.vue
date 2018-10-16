567fb2b1d6fa5c76caee8c40f11754f2e3e1c89c
jakubgania <jakubgania@outlook.com>
2018-10-16 21:24:13 +0200

    update pages/other-resources.vue

--- pages/other-resources.vue
+++ pages/other-resources.vue
@@ -1,12 +1,12 @@
 <template>
   <v-layout row wrap class="projects-layout">
     <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg8 offset-lg2>
-      <div class="" style="font-size:42px;margin-top:140px;text-align:center;">
+      <div class="page-title">
         Inne
       </div>
     </v-flex>
-    <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg8 offset-lg2 style="margin-bottom:80px;">
-      <div class="" style="font-size:24px;margin-top:60px;text-align:center;">
+    <v-flex xs12 sm8 offset-sm2 md6 offset-md3 lg8 offset-lg2>
+      <div class="page-description">
         Ciekawe strony, portale, blogi, oprogramowanie oraz inne zasoby.
       </div>
     </v-flex>
@@ -20,9 +20,9 @@
       offset-md3
       lg6
       offset-lg3
-      style="border-bottom:1px solid #dedede;padding-bottom:10px;"
+      class="links-section"
     >
-      <div class="" style="font-size:22px;padding-top:20px;padding-bottom:20px;letter-spacing:2px;">
+      <div class="link-section-title">
         {{ link.section_name }}
       </div>
       <v-layout row wrap>
@@ -34,13 +34,13 @@
           md12
           lg12
         >
-          <div class="" style="font-size:18px;padding-bottom:10px;">
+          <div class="link-resource-section">
             <a :href="resource.url" target="_blank" class="link-resource">
               {{ resource.title ? index + 1 + ". " + resource.title : index + 1 + ". " + resource.url }}
             </a>
           </div>
-          <div v-if="resource.description" class="">
-            <div class="" style="padding-bottom:20px;font-size:16px;">
+          <div v-if="resource.description">
+            <div class="resource-description">
               {{ resource.description }}
             </div>
           </div>
@@ -83,6 +83,36 @@ export default {
 {
   padding-bottom: 80px;
 }
+.page-title
+{
+  font-size: 42px;
+  margin-top: 140px;
+  text-align: center;
+}
+.page-description
+{
+  font-size: 24px;
+  margin-top: 60px;
+  text-align: center;
+  margin-bottom: 80px;
+}
+.links-section
+{
+  border-bottom: 1px solid #dedede;
+  padding-bottom: 10px;
+}
+.link-section-title
+{
+  font-size: 22px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  letter-spacing: 2px;
+}
+.link-resource-section
+{
+  font-size: 18px;
+  padding-bottom: 10px;
+}
 .link-resource
 {
   text-decoration: none;
@@ -92,12 +122,26 @@ export default {
     color: #01aca7;
   }
 }
+.resource-description
+{
+  padding-bottom: 20px;
+  font-size: 16px;
+}
 @media only screen and (max-width: 600px)
 {
   .projects-layout
   {
     padding-left: 10px;
     padding-right: 10px;
+  }
+  .page-title
+  {
+    margin-top: 60px;
+  }
+  .page-description
+  {
+    margin-top: 40px;
+    margin-bottom: 40px;
   }
 }
 </style>
