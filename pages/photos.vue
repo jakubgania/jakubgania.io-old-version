57f7672d9d872a63f98d0658ac33d21ea72f15db
jakubgania <jakubgania@outlook.com>
2018-10-22 21:45:31 +0200

    updated pages/photos.vue

--- pages/photos.vue
+++ pages/photos.vue
@@ -9,23 +9,50 @@
     </v-flex>
     <v-flex xs12 sm6 offset-sm3 md8 offset-md2 lg8 offset-lg2>
       <div class="image-section" data-aos="fade-up" data-aos-delay="440" data-aos-duration="1200" data-aos-once="true">
-        <div class="" style="font-size:24px;margin-top:60px;text-align:center;">
+        <div class="" style="font-size:24px;text-align:center;">
           Na tej podstronie będą pojawiać się zdjęcia.
         </div>
       </div>
     </v-flex>
     <v-flex xs12 sm10 offset-sm1 md10 offset-md1 lg10 offset-lg1>
-      <v-layout row wrap>
-        <v-flex lg12>
-          images
-        </v-flex>
-      </v-layout>
+      <v-container fluid grid-list-md>
+        <v-layout row wrap>
+          <v-flex
+            xs12
+            sm6
+            md4
+            lg3
+            v-for="(thumbnail, index) in photoDetails"
+            :key="thumbnail.id"
+            class=""
+          >
+            <!-- <v-card tile>
+              <v-card-media
+                :src="photoDetails[index].thumbnail"
+                height="220px"
+              >
+              </v-card-media>
+            </v-card> -->
+            {{ thumbnail }}
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      photoDetails: [
+        'img1',
+        'img2',
+        'img3',
+        'img4'
+      ]
+    }
+  },
   head() {
     return {
       title: 'Jakub Gania - Zdjęcia',
