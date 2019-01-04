6052675ce6531cf6226af3ea9e2b0d994a49c5d2
jakubgania <jakubgania@outlook.com>
2019-01-04 23:43:01 +0100

    create scss/default.scss

--- layouts/default.vue
+++ layouts/default.vue
@@ -50,34 +50,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import '../assets/scss/scrollbar.scss';
-
-.container
-{
-  padding: 0;
-}
-
-@font-face {
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Roboto Regular'), local('Roboto-Regular'),
-  url(../assets/fonts/Roboto-Regular.ttf) format('truetype');
-}
-
-@font-face {
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 700;
-  src: local('Roboto Bold'), local('Roboto-Bold'),
-  url(../assets/fonts/Roboto-Bold.ttf) format('truetype');
-}
-
-@font-face {
-  font-family: 'Nunito';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Nunito Light'), local('Nunito-Light'),
-  url(../assets/fonts/Nunito-Light.ttf) format('truetype');
-}
+  @import '../assets/scss/scrollbar.scss';
+  @import '../assets/scss/default.scss';
 </style>
